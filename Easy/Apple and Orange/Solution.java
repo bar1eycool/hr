652037c89113752c2ca2a67f15f7b5e24e0e6b59
Hsin Hsu <bar1eycool@gmail.com>
2020-04-16 17:37:28 +0800

    written by java

---
+++ Easy/Apple and Orange/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int appleCount = 0;
+        int orangeCount = 0;
+
+        for (int i = 0; i < apples.length; i++) {
+            if (((a + apples[i]) >= s) && ((a + apples[i]) <= t)) {appleCount++;}
+        }
+        
+        for (int i = 0; i < oranges.length; i++) {
+            if (((b + oranges[i]) >= s) && ((b + oranges[i]) <= t)) {orangeCount++;}
+        }
+
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+    }
+}
